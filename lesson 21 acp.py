950208f5ef45020bdefcb63625d569be9fece018
lunar-eclipse26 <sleepysmorz@gmail.com>
2025-10-09 16:55:20 +1300

    Add files via upload

--- lesson 21 acp.py
+++ lesson 21 acp.py
@@ -1,7 +1,6 @@
+
+import datetime
 import calendar
-yy = int(input("enter the current year: "))
-mm = int(input("enter the current month in numbers: "))
-if mm > 12:
-    print("sir pls enter a valid month")
-else:
-    print(calendar.month(yy, mm))
+today = datetime.date.today()
+month = today.month
+print("the current month is:", month)
