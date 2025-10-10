a0e3c69c6f137ce47559309bfdde08dc7d1b3867
lunar-eclipse26 <sleepysmorz@gmail.com>
2025-10-10 16:07:21 +1300

    Add files via upload

--- lesson 21 acp.py
+++ lesson 21 acp.py
@@ -3,4 +3,27 @@ import datetime
 import calendar
 today = datetime.date.today()
 month = today.month
-print("the current month is:", month)
+if month == 1:
+    print("the current month is january")
+elif month == 2:
+    print("the current month is february")
+elif month == 3:
+    print("the current month is march")
+elif month == 4:
+    print("the current month is april")
+elif month == 5:
+    print("the current month is may")
+elif month == 6:
+    print("the current month is june")
+elif month == 7:
+    print("the current month is july")
+elif month == 8:
+    print("the current month is august")
+elif month == 9:
+    print("the current month is september")
+elif month == 10:
+    print("the current month is october")
+elif month == 11:
+    print("the current month is november")
+else:
+    print("the current month is december")
